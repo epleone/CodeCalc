a6623eb0ecb2ca0ec1bf59f37a23ed83fb7a1e45
epleone <epleone@qq.com>
2025-01-03 10:12:44 +0800

    更新发布脚本，删除旧代码

--- public.sh
+++ public.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # 使用git bash 运行
-
-# 检查 release 文件夹是否存在
+# 检查 release 文件夹是否存在并删除
 if [ -d "release" ]; then
-  echo "Error: 'release' folder already exists. Exiting..."
-  exit 1
+  echo "Warning: Deleting existing 'release' folder..."
+  rm -rf release
 fi
 
 # 创建 release 文件夹
