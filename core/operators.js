fad12621869cbc598683e4ca93d0c8c411d6d03a
epleone <epleone@qq.com>
2026-08-06 00:23:10 +0800

    feat: Normalize ×/： symbols

--- core/operators.js
+++ core/operators.js
@@ -923,7 +923,7 @@ export const FUNCTIONS = {
     // 版本号
     'version': {
         args: 0,
-        func: () => 'CodeCalcCore 3.3.0',
+        func: () => 'CodeCalcCore 3.3.1',
         description: 'CodeCalcCore 版本号'
     }
 }; 
